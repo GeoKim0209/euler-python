08cf36c00cfcd30e07aa9ba0158ff9e9ed6d3e8d
Geo Kim <geokim120209@gmail.com>
2025-02-09 13:03:41 +0900

    Project Euler Problem 9 & 10

--- solutions/p10.py
+++ solutions/p10.py
@@ -2,4 +2,8 @@ r"""<p>The sum of the primes below $10$ is $2 + 3 + 5 + 7 = 17$.</p>
 <p>Find the sum of all the primes below two million.</p>"""
 from euler import is_prime
 
-print(is_prime(7))
+x=0
+for i in range(2, 2000001):
+    if is_prime(i):
+        x=x+i
+print(x)
--- solutions/p9.py
+++ solutions/p9.py
@@ -2,3 +2,10 @@ r"""<p>A Pythagorean triplet is a set of three natural numbers, $a \lt b \lt c$,
 $$a^2 + b^2 = c^2.$$</p>
 <p>For example, $3^2 + 4^2 = 9 + 16 = 25 = 5^2$.</p>
 <p>There exists exactly one Pythagorean triplet for which $a + b + c = 1000$.<br>Find the product $abc$.</p>"""
+for a in range(1, 500):
+    for b in range(a + 1, 500):
+        c = 1000 - a - b
+        if a**2 + b**2 == c**2:
+            print("The Pythagorean triplet is:", a, b, c)
+            print("The product abc is:", a * b * c)
+            break
