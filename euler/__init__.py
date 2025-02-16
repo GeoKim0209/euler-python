b8d3c4bd255abdb8d19e11adcf0afea3ab66a545
Geo Kim <geokim120209@gmail.com>
2025-02-16 15:00:46 +0900

    Project Euler Problem 12 WIP

--- euler/__init__.py
+++ euler/__init__.py
@@ -1,8 +1,22 @@
 import math
 
+
 def is_prime(k):
-    
     for i in range(2, int(math.sqrt(k)) + 1):
         if k % i == 0:
             return False
     return True
+
+def get_prime_factorization(n):
+    from collections import Counter
+    r = []
+    while n > 1:
+        factor=n
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                # n is divisible by i
+                factor=i
+                break
+        r.append(factor)
+        n = n // factor
+    return Counter(r)
