9b19aee3c1cf20dc091ffb296b3ba1ac275d23a4
Geo Kim <geokim120209@gmail.com>
2025-04-06 14:47:14 +0900

    Toolkit for getting the sum of divisors of a number

--- euler/__init__.py
+++ euler/__init__.py
@@ -7,16 +7,30 @@ def is_prime(k):
             return False
     return True
 
+
 def get_prime_factorization(n):
     from collections import Counter
+
     r = []
     while n > 1:
-        factor=n
+        factor = n
         for i in range(2, int(math.sqrt(n)) + 1):
             if n % i == 0:
                 # n is divisible by i
-                factor=i
+                factor = i
                 break
         r.append(factor)
         n = n // factor
     return Counter(r)
+
+
+def get_sum_of_divisors(n):
+    # Sum of divisors = (p^0 + p^1 + … + p^m) × (q^0 + q^1 + … + q^n)
+    pf = get_prime_factorization(n)
+    product = 1
+    for prime, exponent in pf.items():
+        sum = 0
+        for i in range(exponent + 1):
+            sum += prime**i
+        product *= sum
+    return product - n
--- solutions/p21.py
+++ solutions/p21.py
@@ -3,21 +3,7 @@ If $d(a) = b$ and $d(b) = a$, where $a \ne b$, then $a$ and $b$ are an amicable 
 <p>For example, the proper divisors of $220$ are $1, 2, 4, 5, 10, 11, 20, 22, 44, 55$ and $110$; therefore $d(220) = 284$. The proper divisors of $284$ are $1, 2, 4, 71$ and $142$; so $d(284) = 220$.</p>
 <p>Evaluate the sum of all the amicable numbers under $10000$.</p>"""
 
-# Sum of divisors = (p^0 + p^1 + … + p^m) × (q^0 + q^1 + … + q^n)
-from euler import get_prime_factorization
 
-print(get_prime_factorization(220))
-
-
-def get_sum_of_divisors(n):
-    pf = get_prime_factorization(n)
-    product = 1
-    for prime, exponent in pf.items():
-        sum = 0
-        for i in range(exponent + 1):
-            sum += prime**i
-        product *= sum
-    return product - n
-
+from euler import get_sum_of_divisors
 
 print(get_sum_of_divisors(284))
