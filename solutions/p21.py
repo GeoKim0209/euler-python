1096344870b2b35bc61cfc5874f697f027d539ee
Geo Kim <geokim120209@gmail.com>
2025-04-06 15:05:23 +0900

    Problem 21 Solved

--- solutions/p21.py
+++ solutions/p21.py
@@ -3,7 +3,15 @@ If $d(a) = b$ and $d(b) = a$, where $a \ne b$, then $a$ and $b$ are an amicable 
 <p>For example, the proper divisors of $220$ are $1, 2, 4, 5, 10, 11, 20, 22, 44, 55$ and $110$; therefore $d(220) = 284$. The proper divisors of $284$ are $1, 2, 4, 71$ and $142$; so $d(284) = 220$.</p>
 <p>Evaluate the sum of all the amicable numbers under $10000$.</p>"""
 
-
 from euler import get_sum_of_divisors
 
-print(get_sum_of_divisors(284))
+d = {}
+
+for i in range(2, 10001):
+    j = get_sum_of_divisors(i)
+    d[i] = j
+sum = 0
+for k, v in d.items():
+    if v in d and d[v] == k and k < v:
+        sum += k + v
+print(sum)
