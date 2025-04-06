c4df052ce79aff9a83b4eb2760b6f6f37127a3ac
Geo Kim <geokim120209@gmail.com>
2025-04-06 14:43:43 +0900

    Problem 18 Finished

--- solutions/p18.py
+++ solutions/p18.py
@@ -28,6 +28,22 @@ s = """3
 7 4
 2 4 6
 8 5 9 3"""
+
+s = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
 split_s = s.split("\n")
 print(f"{split_s=}")
 g = [x.split(" ") for x in split_s]
@@ -60,7 +76,7 @@ def compute_sum(triangle, strategy):
 # Number of possibilities: 'LLL', 'LLR', 'LRL', 'LRR', 'RLL', 'RLR', 'RRL', 'RRR'
 max_sum = 0
 max_x = ""
-for x in itertools.product("LR", repeat=3):
+for x in itertools.product("LR", repeat=14):
     current_sum = compute_sum(final, x)
     if current_sum > max_sum:
         max_sum = current_sum
