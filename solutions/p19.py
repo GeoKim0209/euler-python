8a2a6aff0252d74cd00df9df2edce0a269c921ff
Geo Kim <geokim120209@gmail.com>
2025-04-06 14:44:03 +0900

    Problem 19 Setup

---
+++ solutions/p19.py
@@ -0,0 +1,18 @@
+r"""
+<p>You are given the following information, but you may prefer to do some research for yourself.</p>
+<ul><li>1 Jan 1900 was a Monday.</li>
+<li>Thirty days has September,<br />
+April, June and November.<br />
+All the rest have thirty-one,<br />
+Saving February alone,<br />
+Which has twenty-eight, rain or shine.<br />
+And on leap years, twenty-nine.</li>
+<li>A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.</li>
+</ul><p>How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?</p>"""
+
+def get_day_of_week(year,month,day):
+    #Sunday = 0, Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6
+    pass
+def get_days_difference(year1,month1,day1,year2,month2,day2):
+    pass
+print(get_days_difference(1900,1,1,1901,1,1)) #365
