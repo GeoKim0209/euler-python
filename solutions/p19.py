780fd874a91f30668574404d544a3e4e9ca6a68a
Geo Kim <geokim120209@gmail.com>
2025-04-10 17:17:32 +0900

    Problem 19 Involving Solutions Partly Accounting Dates

--- solutions/p19.py
+++ solutions/p19.py
@@ -10,6 +10,32 @@ And on leap years, twenty-nine.</li>
 <li>A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.</li>
 </ul><p>How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?</p>"""
 
-def get_n_days_after(year,month,day,n):
-    return year,month,day+n
-print(get_n_days_after(1928,12,14,8))
+
+def get_n_days_after(year, month, day, n): 
+    year_after=year
+    month_after=month
+    day_after = day + n
+    maximum_day = get_maximum_day(month)
+    if day_after > maximum_day:
+        month_after = month + 1
+        day_after -= maximum_day
+
+    return year_after, month_after, day_after
+
+
+def get_maximum_day(month):
+    if month in [9, 4, 6, 11]:
+        return 30
+    elif month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month == 2:
+        return 28
+
+
+print(get_n_days_after(1928, 12, 14, 8))  #  1928, 12, 22
+
+print(get_n_days_after(2016, 2, 18, 14))  # 2016, 3, 3
+
+print(get_n_days_after(2017, 2, 25, 5))  # 2017, 3, 2
+
+print(get_n_days_after(1928, 12, 27, 9))  # 1929, 1, 5
